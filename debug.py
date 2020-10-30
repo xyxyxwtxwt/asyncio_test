47cf659f6bc4869c6fcee05417940acbad22db42
xyxwt <xuyan@xyxwtdeMacBook-Pro.local>
2020-10-30 17:02:23 +0800

    add debug.py

--- debug.py
+++ debug.py
@@ -1,4 +1,5 @@
-def add(x,y):
+def add(x, y):
+    res = 0
     for _ in range(20):
         x += 1
         y += 1
@@ -7,5 +8,5 @@ def add(x,y):
     return res
 
 if __name__ == '__main__':
-    res = add(1,1)
+    res = add(1, 1)
     print(res)
